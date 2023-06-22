69cc03e93533b9e97a7a1597abc52de314605bef
Kevin Leary <kevin.n.leary@gmail.com>
2023-06-21 21:19:05 -0400

    fixes and cleanup

--- HW2/HW2-Gnum.c
+++ HW2/HW2-Gnum.c
@@ -26,6 +26,8 @@ struct address_t
 };
 struct address_t *head = NULL;
 
+void createListFromFile();
+void displayList();
 //Functions here
 /*
 
@@ -38,7 +40,7 @@ Per the rurbric:
 2) Look up address
 3) Update address
 4) Delete address
-5) Display list
+5) Display list - almost done 
 6) Display aliases for location
 7) Save to file
 8) Quit 
@@ -48,19 +50,25 @@ Also need a populate list function
 
 int main() {
 
+    //Start with file list...
+    createListFromFile();
+
+    displayList();
+
+    return 0;
+}
+
+void createListFromFile() {
+
     // Open the file
     FILE *address_file = fopen("CS531_Inet.txt", "r+");
     
     // Check if the file was opened successfully.
     if (address_file == NULL) {
         printf("Error opening file.\n");
-        return 1;
+        // return 1;
     }  
     
-     
-    //need starter
-    // struct address_t *ptr = head;
-
     // Read the contents of the file line by line.
     char line[100];
 
@@ -73,42 +81,18 @@ int main() {
         head = test;
     }
 
+}
+
+void displayList() {
     struct address_t *ptr = head;
-    
+
     while (ptr != NULL) {
         printf("%d.%d.%d.%d %s\n", ptr->octet[0], ptr->octet[1], ptr->octet[2], ptr->octet[3], ptr->alias);
         ptr = ptr->next;
     }
 
-    // while (fgets(address_file, "%d.%d.%d.%d %s", &test->octet[0], &test->octet[1], &test->octet[2], &test->octet[3], &test->alias)) {
-    // fscanf(address_file, "%d.%d.%d.%d %s\n", &test->octet[0], &test->octet[1], &test->octet[2], &test->octet[3], test->alias);
-    // printf("%d.%d.%d.%d %s\n", test->octet[0], test->octet[1], test->octet[2], test->octet[3], test->alias);
-    // test->next = head;
-    // head = test;
-    //create new? 
-
-
-
-    // struct test_struct *create_list(intval) { 
-    //     printf("\ncreatinglistwithheadnodeas[%d]\n",val); 
-    //     struct test_struct *ptr = (struct test_struct*)m alloc(sizeof(struct test_struct));
-    //      if(NULL==ptr) { 
-    //          printf("\nNodecreationfailed\n"); 
-    //          returnNULL; 
-    //     } 
-    //     ptr->val=val; 
-    //     ptr->next=NULL; 
-    //     head=curr=ptr; 
-    //     return ptr; 
-    // }
-    
-    // printf("%s\n", line);
-    //   i=0;
-        // i++;
-    // }   
-
-    // Close the file.
-    // fclose(address_file);
-
-    return 0;
+    if (head == NULL) {
+        printf("Address list is NULL!\n");
+    }
+    //TODO - Reprint menu option
 }
